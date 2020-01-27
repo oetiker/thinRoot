26227faafa7353a616e94d492f10a8958b30af8f
Tobias Oetiker <tobi@oetiker.ch>
2020-01-27 09:56:55 +0100

    added default config

---
+++ boot-server/sample-data/overlay/bin/thinlinc-startup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd $HOME
+xterm&
+MAC_ADDR=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address)
+/usr/bin/curl ${BASE_PATH}/home.pkg?mac=${MAC_ADDR} | /bin/tar -zxf -
+/bin/tlclient
+/bin/tar zcf /tmp/$$.tar.gz .thinlinc/* && /usr/bin/curl -F data=@/tmp/$$.tar.gz ${BASE_PATH}/home.pkg?mac=${MAC_ADDR}
+rm /tmp/$$.tar.gz
