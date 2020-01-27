8ec007b8f2fb760d51db019e2d3d9e7fd9432fa0
Tobias Oetiker <tobi@oetiker.ch>
2020-01-27 09:29:00 +0100

    tuned for op thin

---
+++ boot-server/lib/BootServer.pm
@@ -0,0 +1,102 @@
+package BootServer;
+use Mojo::Base 'Mojolicious', -signatures;
+use Mojo::Util qw(dumper);
+# This method will run once at server start
+
+
+
+has cfg => sub ($self) {
+     # Load configuration from hash returned by config file
+    $self->plugin('Config', 
+        file => $self->home->rel_file('etc/boot-server.cfg')
+    );
+};
+
+sub startup {
+    my $self = shift;
+    # Configure the application
+    $self->secrets($self->cfg->{secrets});
+    # Router
+    $self->helper( getDir => sub ($c,$name,$new = 0) {
+        my $ip = $c->param('mac') || $c->tx->remote_address;
+        my $root = $self->cfg->{dataRoot};
+        my $dst = $root.'/'.$name;
+        if ($new or -e $dst.'__'.$ip){
+            $dst .= '__'.$ip;
+        }
+        return $dst;
+    });
+    my $r = $self->routes;
+    my $root = $self->cfg->{dataRoot};
+    # Normal route to controller
+    $r->get('ipxe.cfg' => sub ($c) {
+        $c->render(text => <<CFG_END, format => 'txt' );
+#!ipxe
+set base http://froburg.oetiker.ch:3833
+# note the BOOT_IMAGE parameter is used to identify the location from where to get the rest of the configuration from
+kernel \${base}/bzImage BOOT_IMAGE=\${base}/bzImage console=tty2 kgdboc=tty2 noswap elevator=deadline consoleblank=120 quiet loglevel=0 vga=775
+boot
+CFG_END
+    });
+    $r->get('/bzImage' => sub ($c) {
+        my $bzImage = $c->getDir('bzImage');
+        $c->log->debug("Shipping $bzImage");
+        my $asset = Mojo::Asset::File->new(path => $bzImage);
+        $c->reply->asset($asset);
+    });
+    $r->get('/conf/thinroot.conf.network' => sub ($c) {
+        $c->render( text => <<CFG_END );
+SESSION_0_QUTSELECT_CMD=/bin/thinlinc-startup.sh
+CFG_END
+    });
+    $r->get('/<archive>.pkg' => [ archive => [qw(overlay home)] ] => sub ($c) {
+        my $name = $c->stash('archive');
+        my $dir = $c->getDir($name);
+        # Operation that would block the event loop for 5 seconds
+        my $sp = Mojo::IOLoop->subprocess( sub ($sp) {  
+            open my $tar, '-|','tar','-C',$dir,'-zcf','-','.'
+                // die "Problem with tar $!";
+            my $buffer;
+            while (my $bytesread = $tar->read($buffer,1024*1024)) {    
+                $sp->progress($buffer);
+            }
+            close $tar;
+            return $?;
+        }, sub  ($sp, $err, $data) {
+            if ($err) {
+                $c->log->error($err);
+                $c->render( status => 500, text => $err);
+            }
+            $c->finish;
+        });
+        $sp->on('progress' => sub ($sp,$data) {
+            $c->write($data);
+        });
+        $c->render_later;
+    });
+    $r->post('/home.pkg' => sub ($c) {
+        return $c->render(text => 'File is too big.', status => 500)
+            if $c->req->is_limit_exceeded;
+        return $c->render(text => 'Expected an upload in data.', status => 500)     if ref $c->param('data') ne 'Mojo::Upload';
+        my $dir = $c->getDir('home',1);
+        my $data = $c->param('data')->slurp;
+        my $sp=  Mojo::IOLoop->subprocess( sub ($sp) {         
+            mkdir $dir if not -d $dir;
+            open my $tar, '|-', 'tar','-C',$dir,'-zxf','-' 
+                // die "Problem with tar $!";
+            print $tar $data;
+            close($tar);
+            return $?;
+        },
+        sub  ($sp, $err, @data) {
+            if ($err) {
+                $c->log->error($err);
+                $c->render( status => 500, text => $err);
+            }
+            $c->render( status => 200, text => $data[1]);
+        });
+        $c->render_later;
+    });
+}
+
+1;
---
+++ buildroot-external/package/remmina/remmina.mk
@@ -0,0 +1,42 @@
+################################################################################
+#
+# remmina
+#
+################################################################################
+
+REMMINA_VERSION = v1.3.10
+REMMINA_SITE = https://gitlab.com/Remmina/Remmina/-/archive/$(REMMINA_VERSION)/Remmina-$(REMMINA_VERSION).tar.gz
+REMMINA_LICENSE = GPLv2+ with OpenSSL exception
+REMMINA_LICENSE_FILES = COPYING LICENSE LICENSE.OpenSSL
+
+REMMINA_CONF_OPTS = \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DWITH_SSE2=ON \
+	-DWITH_CUPS=off \
+	-DWITH_SPICE=OFF \
+	-DWITH_WAYLAND=off \
+	-DWITH_PULSE=on \
+	-DWITH_AVAHI=OFF \
+	-DWITH_APPINDICATOR=OFF \
+	-DWITH_GNOMEKEYRING=OFF \
+	-DWITH_TELEPATHY=OFF \
+	-DWITH_GCRYPT=OFF \
+	-DWITH_LIBSSH=OFF \
+	-DWITH_VTE=OFF
+
+REMMINA_DEPENDENCIES = \
+	libgtk3 libvncserver freerdp \
+	xlib_libX11 xlib_libXext xlib_libxkbfile
+
+ifeq ($(BR2_NEEDS_GETTEXT),y)
+REMMINA_DEPENDENCIES += gettext
+
+define REMMINA_POST_PATCH_FIXINTL
+	$(SED) 's/$${GTK_LIBRARIES}/$${GTK_LIBRARIES} -lintl/' \
+		$(@D)/remmina/CMakeLists.txt
+endef
+
+REMMINA_POST_PATCH_HOOKS += REMMINA_POST_PATCH_FIXINTL
+endif
+
+$(eval $(cmake-package))
