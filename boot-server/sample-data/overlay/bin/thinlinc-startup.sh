02ca76e768aefe80c3752c5a58b1fca2744304f8
Tobias Oetiker <tobi@oetiker.ch>
2020-01-30 08:19:51 +0100

    backup remmina config too

--- boot-server/sample-data/overlay/bin/thinlinc-startup.sh
+++ boot-server/sample-data/overlay/bin/thinlinc-startup.sh
@@ -5,5 +5,5 @@ pkill -0 -f remmina || remmina &
 MAC_ADDR=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address)
 /usr/bin/curl ${BASE_PATH}/home.pkg?mac=${MAC_ADDR} | /bin/tar -zxf -
 /bin/tlclient
-/bin/tar zcf /tmp/$$.tar.gz .thinlinc/* && /usr/bin/curl -F data=@/tmp/$$.tar.gz ${BASE_PATH}/home.pkg?mac=${MAC_ADDR}
+/bin/tar zcf /tmp/$$.tar.gz .thinlinc .local/share/remmina && /usr/bin/curl -F data=@/tmp/$$.tar.gz ${BASE_PATH}/home.pkg?mac=${MAC_ADDR}
 rm /tmp/$$.tar.gz
