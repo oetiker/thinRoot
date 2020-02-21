31c379a3ae3da3b8720985280fa5c7f418b465c1
Tobias Oetiker <tobi@oetiker.ch>
2020-02-21 08:24:48 +0100

    fix startup and enable

--- boot-server/sample-data/overlay/bin/thinlinc-startup.sh
+++ boot-server/sample-data/overlay/bin/thinlinc-startup.sh
@@ -5,5 +5,6 @@ pkill -0 -f remmina || remmina &
 MAC_ADDR=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address)
 /usr/bin/curl ${BASE_PATH}/home.pkg?mac=${MAC_ADDR} | /bin/tar -zxf -
 /bin/tlclient
-/bin/tar zcf /tmp/$$.tar.gz .thinlinc .local/share/remmina && /usr/bin/curl -F data=@/tmp/$$.tar.gz ${BASE_PATH}/home.pkg?mac=${MAC_ADDR}
+mkdir -p .config/freerdp .config/remmina .local/share/remmina .thinlinc
+/bin/tar zcf /tmp/$$.tar.gz .config/freerdp .config/remmina .local/share/remmina .thinlinc && /usr/bin/curl -F data=@/tmp/$$.tar.gz ${BASE_PATH}/home.pkg?mac=${MAC_ADDR}
 rm /tmp/$$.tar.gz
